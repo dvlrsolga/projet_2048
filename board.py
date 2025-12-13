851a278554fd38d481aff60ba706b56ad3108841
Tim <tim.brusten@student.uclouvain.be>
2025-12-13 10:52:34 +0100

    first working class

--- board.py
+++ board.py
@@ -1,1 +1,18 @@
-#je t'aime guinou
+class Board:
+
+    def __init__(self):
+        self.matrix = [[0 for i in range(4)] for j in range(4)]
+
+        
+
+    def __str__(self):
+        string = ""
+        for i in range(3):
+            string += str(self.matrix[i][0]) + " " + str(self.matrix[i][1]) +" "+ str(self.matrix[i][2]) +" "+ str(self.matrix[i][3])+"\n"
+        string+= str(self.matrix[3][0]) + " " + str(self.matrix[3][1]) +" "+ str(self.matrix[3][2]) +" "+ str(self.matrix[3][3])
+        
+        return string
+    
+
+board1 = Board()
+print(board1)
